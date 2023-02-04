c46918ad2c805e14a2cc14d14f7c8310b15d5c29
rutvajmehta <70297761+rutvajmehta@users.noreply.github.com>
2023-02-04 18:44:20 -0500

    fixed syntax

--- backend/env/randomized_digits_in_CSV.py
+++ backend/env/randomized_digits_in_CSV.py
@@ -3,8 +3,8 @@ import csv
 def main():
     with open('companies.csv', 'w', newline='') as file:
        writer = csv.writer(file)
-       for x in 100:
-           writer.writerow(["name", "Men's Average Salary", "Women's Average Salary"])
+       for x in range(100):
+           writer.writerows(["name", "Men's Average Salary", "Women's Average Salary"])
 
 if __name__ == "__main__":
     main()
