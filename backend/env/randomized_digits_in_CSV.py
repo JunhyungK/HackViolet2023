d5857eff974950f2682227abb7f711e28e98d0bf
rutvajmehta <70297761+rutvajmehta@users.noreply.github.com>
2023-02-04 19:55:26 -0500

    got csv file to work

--- backend/env/randomized_digits_in_CSV.py
+++ backend/env/randomized_digits_in_CSV.py
@@ -1,10 +1,12 @@
 import csv
+import random
 
 def main():
     with open('companies.csv', 'w', newline='') as file:
-       writer = csv.writer(file)
-       for x in range(100):
-           writer.writerows(["name", "Men's Average Salary", "Women's Average Salary"])
+        writer = csv.writer(file)
+        writer.writerow(['Name', 'Mens Average Salary', 'Womens Average Salary'])
+        for x in range(100):
+            writer.writerow([x, random.randint(150, 250), random.randint(100, 200)])
     return writer
 
 if __name__ == "__main__":
